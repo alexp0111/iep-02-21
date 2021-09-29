f7dceb757760cadc7a965bf046447482e39c61a7
Alexx <alexp0111"mail.ru>
2021-09-29 23:05:52 +0300

    Create user class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,15 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-	    int a, b;
-        a = in.nextInt();
-        b = 3;
-        System.out.println(a+b);
-        System.out.println("hw");
+        User user = new User();
+        System.out.println("Enter your name, age and group:");
+            user.setName(in.next());
+            user.setAge(in.next());
+            user.setGroup(in.next());
+        System.out.println("User " + user.getName() + " has created");
     }
 }
---
+++ src/com/company/User.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class User {
+    String name, age, group;
+
+    public User() {
+    }
+
+    public User(String name, String age, String group) {
+        this.name = name;
+        this.age = age;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+}
