296637b7f76923537a04f0918b04c1d7ecf4fa72
Alexx <alexp0111"mail.ru>
2021-09-29 17:57:19 +0300

    init java project

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+	    int a, b;
+        a = in.nextInt();
+        b = 3;
+        System.out.println(a+b);
+        System.out.println(a+b+1);
+        System.out.println(a+b+2);
+    }
+}
