0b4d224cdb46689f55166c69c970ffe4458b52d4
Alexander <alexp0111@mail.ru>
2021-10-06 17:48:23 +0300

    elif and while

--- src/samsung_ac_project/Main.java
+++ src/samsung_ac_project/Main.java
@@ -1,8 +1,29 @@
 package samsung_ac_project;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static int read(Scanner v) {
+        return v.nextInt();
+    }
+
     public static void main(String[] args) {
-	// lesson_2
+        Scanner i = new Scanner(System.in);
+        int t1, t2, sum;
+        //System.out.println("Enter number:");
+        t1 = read(i);
+        t2 = read(i);
+        sum = t1 + t2;
+        if (sum == 100) {
+            System.out.println("it is 100");
+        } else if (sum > 100) {
+            System.out.println("Bigger");
+        } else {
+            while (sum < 100){
+                sum += 1;
+                System.out.println("Sum is: " + sum);
+            }
+        }
     }
 }
